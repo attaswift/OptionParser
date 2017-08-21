a455c15832008eee05a9e54542c0425c52521891
Károly Lőrentey <karoly@lorentey.hu>
2017-08-21 18:27:25 +0200

    Initial Commit

---
+++ OptionParser/OptionParser.h
@@ -0,0 +1,19 @@
+//
+//  OptionParser.h
+//  OptionParser
+//
+//  Created by Károly Lőrentey on 2017-08-21.
+//  Copyright © 2017 Károly Lőrentey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OptionParser.
+FOUNDATION_EXPORT double OptionParserVersionNumber;
+
+//! Project version string for OptionParser.
+FOUNDATION_EXPORT const unsigned char OptionParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OptionParser/PublicHeader.h>
+
+
